ab4b69a06c68c9fa0e7a0638df55e12c959e51db
RowDog <mrdiamonddirt@gmail.com>
2022-11-30 10:32:32 +0000

    config es lint and prettier --save-dev eslint-config-prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,19 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
+  env: {
+    browser: true,
+    es2021: true,
   },
-  'extends': [
-    'plugin:react/recommended',
-    'google',
-  ],
-  'overrides': [
-  ],
-  'parserOptions': {
-    'ecmaVersion': 'latest',
-    'sourceType': 'module',
+  extends: ["plugin:react/recommended", "google"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  'plugins': [
-    'react',
-  ],
-  'rules': {
+  plugins: ["react"],
+  settings: {
+    react: {
+      version: "latest",
+    },
   },
+  rules: {},
 };
